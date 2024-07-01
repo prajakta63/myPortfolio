27336b9e2dde85aa2f8436cec100b72f3eb77255
Prajakta Tamse <pruthavepraju@gmail.com>
2024-07-01 22:18:58 +0530

    portfolio files modified

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,10 +6,8 @@ export const Contact = () => {
   return (
     <footer id='contact' className={styles.container}>
         <div className={styles.text}>
-            <h2>
-                Contact
-            </h2>
-            <p> Feel free to reach out</p>
+            <h2>Contact Me</h2>
+            <p>Let's get in touch</p>
         </div>
         <ul className={styles.links}>
             <li className={styles.link}>
