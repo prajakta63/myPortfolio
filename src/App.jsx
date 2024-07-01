2297b2a1f477c33652821fa97b1e6286cb57e500
Prajakta Tamse <pruthavepraju@gmail.com>
2024-07-01 23:06:41 +0530

    portfolio files modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { About } from './components/About/About';
 import {Hero} from './components/Hero/Hero';
 import {Navbar} from './components/Navbar/Navbar';
 import {Experience} from './components/Experience/Experience';
-// import {Projects} from './components/Projects/Projects';
+import {Projects} from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
     <Hero />
     <About />
     <Experience />
-    {/* <Projects /> */}
+    <Projects />
     <Contact />
   </div>)
 
